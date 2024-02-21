a0fd4f7a39d910c6ef932af10d8f62a5b111ebe6
Alvaro Villares <alvarovillaresrodriguez@gmail.com>
2024-02-21 19:29:20 +0100

    Funcionalidad de cazar al topo terminada

--- JuegoPulsarAlvaro/MainWindow.xaml.cs
+++ JuegoPulsarAlvaro/MainWindow.xaml.cs
@@ -29,32 +29,78 @@ namespace JuegoPulsarAlvaro
         private const int VELOCIDAD_RAPIDA = 1000;
         private int velocidad = VELOCIDAD_LENTA;
 
-        private Button[] botones = [];
+        private Button[] botones;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            botones = new Button[] { Boton1, Boton2, Boton3, Boton4, Boton5, Boton6 };
+
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(velocidad);
 
             // Iniciar el temporizador
             timer.Start();
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (Button boton in botones)
+            {
+                boton.Opacity = 0.5;
+            }
+            Random random = new Random();
+            int indiceAleatorio = random.Next(botones.Length);
+            botones[indiceAleatorio].Opacity = 1;
+        }
+
         private void Lenta_Click(object sender, RoutedEventArgs e)
         {
             velocidad = VELOCIDAD_LENTA;
+            timer.Stop();
+            timer.Interval = TimeSpan.FromMilliseconds(velocidad);
+            timer.Start();
         }
 
         private void Media_Click(object sender, RoutedEventArgs e)
         {
             velocidad = VELOCIDAD_MEDIA;
+            timer.Stop();
+            timer.Interval = TimeSpan.FromMilliseconds(velocidad);
+            timer.Start();
         }
 
         private void Rapida_Click(object sender, RoutedEventArgs e)
         {
             velocidad = VELOCIDAD_RAPIDA;
+            timer.Stop();
+            timer.Interval = TimeSpan.FromMilliseconds(velocidad);
+            timer.Start();
+        }
+
+        private void Boton_Click(object sender, RoutedEventArgs e)
+        {
+            Button botonPulsado = sender as Button;
+            if (botonPulsado != null && botonPulsado.Opacity == 1)
+            {
+                timer.Stop();
+
+                botonPulsado.Opacity = 0.5;
+
+                Random random = new Random();
+                int indiceAleatorio = random.Next(botones.Length);
+                botones[indiceAleatorio].Opacity = 1;
+
+                timer.Interval = TimeSpan.FromMilliseconds(velocidad);
+                timer.Start();
+            }
+        }
+
+        private void Boton1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
